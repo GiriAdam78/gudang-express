64580fb4885ae3c8e5561dfdf1f84d9def2c175e
Giridiwa319 <giriadam67@gmail.com>
2024-12-18 16:19:40 +0700

    Update Route Api

--- index.js
+++ index.js
@@ -8,12 +8,34 @@ const PORT = 3500;
 
 app.use(bodyParser.json());
 
+// Home Page
 app.get('/', (req, res) => {
     res.json({
         message:"Gudangku"
     })
 })
 
+// Post Gudang
+app.post('/gudang', async(req, res) => {
+    const {
+        nama,
+        alamat,
+        kapasitas
+    } = req.body;
+    try{
+        const gudang = await prisma.gudang.create({
+            data:{
+                nama,
+                alamat,
+                kapasitas
+            }
+        });
+
+        res.json(gudang);
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+})
 app.listen(PORT, () => {
     console.log(`✅ Server is running on port ${PORT}`);
 })
